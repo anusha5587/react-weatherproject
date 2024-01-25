fffc217af75970aa7e86f1cf14391d8f2e6ed15b
anusha5587 <anoosha.anand.24@gmail.com>
2024-01-25 22:56:24 +0000

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export default function App() {
   return (
     <div className="App">
       <h1 class="headingText">Let's check the weather!</h1>
+
+      <footer>
+        This project was coded by Anoosha Anand and is open-soursed on{" "}
+        <a
+          href="https://github.com/anusha5587/react-weatherproject"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
